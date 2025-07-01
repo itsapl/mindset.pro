825a4b6e327a60bc279308c33c3fdc48b8eea878
itsapl <96710638+itsapl@users.noreply.github.com>
2025-07-01 05:44:41 +0200

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -1,3 +1,4 @@
+// Ten kod działa na serwerze Netlify, a nie w przeglądarce użytkownika.
 exports.handler = async function(event) {
   // Akceptuj tylko zapytania metodą POST
   if (event.httpMethod !== 'POST') {
